50a585e812a8267b41945370961c14050c5ca542
lorenzofavaro <lorysdl@gmail.com>
2018-11-24 22:07:58 +0100

    Aggiunti DAO, modificato Professore -> Docente

--- src/main/java/dao/CorsoDAO.java
+++ src/main/java/dao/CorsoDAO.java
@@ -1,4 +1,67 @@
 package dao;
 
+import connection.DBConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class CorsoDAO {
+    public static void insert(String titolo) throws SQLException {
+        String insert = "INSERT INTO corso(titolo) VALUES(?)";
+        PreparedStatement st = null;
+        Connection conn = DBConnection.getInstance();
+        try {
+            st = conn.prepareStatement(insert);
+            st.setString(1, titolo);
+            st.executeUpdate();
+        }finally {
+            if (st != null) st.close();
+            if (conn != null) conn.close();
+        }
+    }
+
+    public static void remove(String titolo) throws SQLException {
+        String remove = "DELETE FROM corso WHERE titolo = ?";
+        PreparedStatement st = null;
+        Connection conn = DBConnection.getInstance();
+        try {
+            st = conn.prepareStatement(remove);
+            st.setString(1, titolo);
+            st.executeUpdate();
+        }finally {
+            if (st != null) st.close();
+            if (conn != null) conn.close();
+        }
+    }
+
+    public static void update(String titoloInit, String titoloFinal) throws SQLException {
+        String modify = "UPDATE corso SET titolo = ? WHERE titolo = ?";
+        PreparedStatement st = null;
+        Connection conn = DBConnection.getInstance();
+        try {
+            st = conn.prepareStatement(modify);
+            st.setString(1, titoloFinal);
+            st.setString(2, titoloInit);
+            st.executeUpdate();
+        }finally {
+            if (st != null) st.close();
+            if (conn != null) conn.close();
+        }
+    }
+
+    public static int getN() throws SQLException {
+        String getN = "SELECT count(*) FROM corso";
+        PreparedStatement st = null;
+        Connection conn = DBConnection.getInstance();
+        try {
+            st = conn.prepareStatement(getN);
+            ResultSet rs = st.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        }finally {
+            if (st != null) st.close();
+            if (conn != null) conn.close();
+        }
+    }
 }
